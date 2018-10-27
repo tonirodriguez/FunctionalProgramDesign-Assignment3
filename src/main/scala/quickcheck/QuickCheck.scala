a01edfa21287314ceb2400ed84b11cef9ce31588
Toni Rodriguez <arodriguezt@gmail.com>
2018-10-27 22:21:12 +0200

    Heap generator

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -9,7 +9,13 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for {
+      i <- arbitrary[A]
+      h <- oneOf(const(empty), genHeap)
+    } yield insert(i, h)
+  )
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
